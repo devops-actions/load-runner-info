954491186908ac1cba6d2eaa356903a643d6e9de
Rob Bos <raj.bos@gmail.com>
2022-01-05 17:06:13 +0100

    first draft of the action

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # load-runner-info
 Load the information for the runners that are available
+
+## Inputs
+
+|Name|Type|Description|
+|---|---|---|
+|`organization`|string|The slug of the organization to load the runners from|
+|`repo`|string|The slug of the repo to load the runners from, `organization` required as well|
+|`accessToken`|string|The access token to use to connect to the GitHub API|
+
+### Access token information
+To run this action at the **organization** level, the access token must have scope `admin:org`
+To run this action at the **repository** level, the access token must have scope `owner:repo`
+
+## Outputs
+
+|Name|Type|Description|
+|---|---|---|
+|`runners`|string|A JSON string with the runner information available|
+
+Example:
+``` json
+{
+    "total_count": 1,
+    "runners": [
+        {
+            "id": 2,
+            "name": "my-runner",
+            "os": "windows",
+            "status": "online",
+            "busy": false,
+            "labels": [
+                {
+                    "id": 1,
+                    "name": "self-hosted",
+                    "type": "read-only"
+                },
+                {
+                    "id": 2,
+                    "name": "Windows",
+                    "type": "read-only"
+                },
+                {
+                    "id": 3,
+                    "name": "X64",
+                    "type": "read-only"
+                }
+            ]
+        }
+    ]
+}
+```
---
+++ src/main.ts
@@ -0,0 +1,75 @@
+import * as core from '@actions/core'
+import {Octokit} from 'octokit'
+import dotenv from 'dotenv'
+
+// always import the config
+dotenv.config()
+
+async function run(): Promise<void> {
+  console.log('Starting')
+
+  const accessToken = core.getInput('accessToken') || process.env.ACCESS_TOKEN || ''
+    const repo = core.getInput('repo') || process.env.GITHUB_REPO || ''
+    const organization =
+      core.getInput('organization') || process.env.GITHUB_ORGANIZATION || ''
+
+    if (!accessToken || accessToken === '') {
+      core.setFailed(
+        "Parameter 'accessToken' is required to load all actions from the organization or user account"
+      )
+      return
+    }
+
+    if (organization === '') {
+      core.setFailed(
+        "Parameter 'organization' is required to load all runners from it. Please provide one of them."
+      )
+      return
+    }
+
+    const octokit = new Octokit({auth: accessToken})
+
+    let runnerInfo: any = {}
+    if (organization !== '' && repo === '') {
+      try {
+        console.log(`Loading all runners from organization [${organization}]`)
+        const { data } = await octokit.request("GET /orgs/{owner}/actions/runners", {
+          owner: organization
+        })
+
+        console.log(`Found ${data.total_count} runners at the org level`)
+        runnerInfo = data
+      } catch (error) {
+        console.log(error)
+        core.setFailed(
+          `Could not authenticate with PAT. Please check that it is correct and that it has [read access] to the organization or user account: ${error}`
+        )
+        return
+      }
+    }
+
+    if (repo !== '') {
+      try {
+        console.log(`Loading all runners from repo [${organization}/${repo}]`)
+        const { data } = await octokit.request("GET /repos/{owner}/{repo}/actions/runners", {
+          owner: organization,
+          repo
+        })
+
+        console.log(`Found ${data.total_count} runners at the repo level`)
+        runnerInfo = data
+      } catch (error) {
+        console.log(error)
+        core.setFailed(
+          `Could not authenticate with PAT. Please check that it is correct and that it has [read access] to the organization or user account: ${error}`
+        )
+        return
+      }
+    }
+
+    console.log(`Found ${runnerInfo.total_count} runners`)
+    const json = JSON.stringify(runnerInfo)
+    core.setOutput('runners', json)
+}
+
+run()
